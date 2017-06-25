1b9191cef19df9148418844ed0085595fc52f315
Chiao Fu <joannelovecat@gmail.com>
2017-06-25 22:17:09 +0800

    O(1) memory

--- 160.cpp
+++ 160.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,26 +9,12 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        vector<ListNode*> A, B;
-        ListNode *current;
-        // save all nodes
-        current = headA;
-        while (current != NULL) {
-            A.push_back(current);
-            current = current -> next;
+        ListNode *current1 = headA, *current2 = headB;
+        // current1: A -> B, current2: B -> A, they will be same length and intersection will be at same number of node
+        while (current1 != current2) {
+            current1 = (current1 != NULL) ? current1 -> next : headB;
+            current2 = (current2 != NULL) ? current2 -> next : headA;
         }
-        current = headB;
-        while (current != NULL) {
-            B.push_back(current);
-            current = current -> next;
-        }
-        // find the intersection from tail
-        if (A.size() == 0 || B.size() == 0 || A.back() != B.back())
-            return NULL;
-        for (int i = 0; i < min(A.size(), B.size()); i++) {
-            if (A[A.size() - 1 - i] != B[B.size() - 1 - i])
-                return A[A.size() - i];
-        }
-        return (A.size() < B.size()) ? A[0] : B[0];
+        return current1;
     }
 };
