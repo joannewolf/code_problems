329a19aafee7ce8338be4fac151db86c7098484a
Chiao Fu <joannelovecat@gmail.com>
2017-06-29 22:04:46 +0800

    without loop

--- 326.cpp
+++ 326.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n <= 0)
-        	return false;
-        while (n != 1) {
-        	if (n % 3 != 0)
-        		return false;
-        	n /= 3;
-        }
-        return true;
+        // 1162261467 = 3^19, 3^20 will overflow
+        return (n > 0 && 1162261467 % n == 0);
     }
 };
