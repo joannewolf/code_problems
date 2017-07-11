f88479a272e5d8bfb37c67b870666dcf7d83107d
Chiao Fu <joannelovecat@gmail.com>
2017-07-11 11:43:59 +0800

    by iteration

--- 101.cpp
+++ 101.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -8,19 +9,42 @@
  * };
  */
 class Solution {
-private:
-    bool isSymmetricNode(TreeNode *left, TreeNode *right) {
-        if (left == NULL && right == NULL)
-            return true;
-        else if ((left != NULL && right == NULL) || (left == NULL && right != NULL) || (left -> val != right -> val))
-            return false;
-        else
-            return (isSymmetricNode(left -> left, right -> right) & isSymmetricNode(left -> right, right -> left));
-    }
 public:
     bool isSymmetric(TreeNode* root) {
         if (root == NULL)
             return true;
-        return isSymmetricNode(root -> left, root -> right);
+        stack<TreeNode*> left, right;
+
+        if (root -> left == NULL && root -> right == NULL)
+            return true;
+        else if (root -> left != NULL && root -> right != NULL) {
+            left.push(root -> left);
+            right.push(root -> right);
+        }
+        else
+            return false;
+
+        while (!left.empty() && !right.empty()) {
+            if (left.top() -> val != right.top() -> val)
+                return false;
+            else {
+                TreeNode *tempLeft = left.top(), *tempRight = right.top();
+                left.pop();
+                right.pop();
+                if (tempLeft -> left != NULL && tempRight -> right != NULL) {
+                    left.push(tempLeft -> left);
+                    right.push(tempRight -> right);
+                }
+                else if ((tempLeft -> left == NULL) != (tempRight -> right == NULL))
+                    return false;
+                if (tempLeft -> right != NULL && tempRight -> left != NULL) {
+                    left.push(tempLeft -> right);
+                    right.push(tempRight -> left);
+                }
+                else if ((tempLeft -> right == NULL) != (tempRight -> left == NULL))
+                    return false;
+            }
+        }
+        return true;
     }
 };
