fa9077af1379248b75aca0ccccc35082f5707088
Chiao Fu <joannelovecat@gmail.com>
2017-07-12 18:36:32 +0800

    top down

--- 107.cpp
+++ 107.cpp
@@ -9,33 +9,22 @@
  * };
  */
 class Solution {
+private:
+    vector<vector<int>> nodeList;
+    void printOrder(int level, TreeNode *node) {
+        if (node == NULL)
+            return;
+        if (nodeList.size() < level)
+            nodeList.insert(nodeList.begin(), vector<int>());
+        nodeList[nodeList.size() - level].push_back(node -> val);
+        if (node -> left != NULL)
+            printOrder(level + 1, node -> left);
+        if (node -> right != NULL)
+            printOrder(level + 1, node -> right);
+    }
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>> result, leftResult, rightResult;
-        int n, ln, rn;
-
-        if (root == NULL)
-            return result;
-        if (root -> left == NULL && root -> right == NULL) {
-            result.insert(result.begin(), vector<int>(1, root -> val));
-            return result;
-        }
-
-        if (root -> left != NULL)
-            leftResult = levelOrderBottom(root -> left);
-        if (root -> right != NULL)
-            rightResult = levelOrderBottom(root -> right);
-        ln = leftResult.size();
-        rn = rightResult.size();
-        result.insert(result.begin(), vector<int>(1, root -> val));
-        vector<vector<int>> temp(max(ln, rn), vector<int>());
-        result.insert(result.begin(), temp.begin(), temp.end());
-        n = result.size();
-        for (int i = 0; i < leftResult.size(); i++)
-            result[n - 2 - i].insert(result[n - 2 - i].end(), leftResult[ln - 1 - i].begin(), leftResult[ln - 1 - i].end());
-        for (int i = 0; i < rightResult.size(); i++)
-            result[n - 2 - i].insert(result[n - 2 - i].end(), rightResult[rn - 1 - i].begin(), rightResult[rn - 1 - i].end());
-        
-        return result;
+        printOrder(1, root);
+        return nodeList;
     }
 };
