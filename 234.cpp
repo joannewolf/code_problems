ad1de743e0fb6fa0f12c2062a0b6f904a28dff64
Chiao Fu <joannelovecat@gmail.com>
2017-07-16 18:46:14 +0800

    O(1) space

--- 234.cpp
+++ 234.cpp
@@ -7,17 +7,40 @@
  * };
  */
 class Solution {
+private:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head -> next == NULL)
+            return head;
+        ListNode *previous = NULL, *current = head, *next = head -> next;
+        while (next != NULL) {
+            current -> next = previous;
+            previous = current;
+            current = next;
+            next = next -> next;
+        }
+        current -> next = previous;
+
+        return current;
+    }
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> nums;
-        ListNode *current = head;
-        while (current != NULL) {
-            nums.push_back(current -> val);
-            current = current -> next;
+        if (head == NULL || head -> next == NULL)
+            return true;
+        // use slow and fast pointer to find the middle node
+        ListNode *slow = head, *fast = head;
+        while (fast != NULL && fast -> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
         }
-        for (int i = 0; i < nums.size() / 2; i++) {
-            if (nums[i] != nums[nums.size() - 1 - i])
+        if (fast != NULL)
+            slow = slow -> next;
+
+        ListNode *left = head, *right = reverseList(slow);
+        while (right != NULL) {
+            if (left -> val != right -> val)
                 return false;
+            left = left -> next;
+            right = right -> next;
         }
         return true;
     }
