956cae356a86f99927c33cb99b63efd2bec84694
Chiao Fu <joannelovecat@gmail.com>
2017-07-26 09:25:19 +0800

    O(logN)

--- 33.cpp
+++ 33.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target)
-                return i;
+        int l = 0, r = nums.size() - 1, rotate;
+        // find index of smallest value, same as the rotate pivot
+        while (l < r) {
+        	int middle = (l + r) / 2;
+        	if (nums[middle] > nums[r])
+        		l = middle + 1;
+        	else
+        		r = middle;
         }
+        rotate = l;
+        printf("rotate %d\n", rotate);
+
+        // find index of target
+        l = 0, r = nums.size() - 1;
+        while (l <= r) {
+        	int middle = (l + r) / 2;
+        	int realMiddle = (middle + rotate) % nums.size();
+        	if (nums[realMiddle] == target)
+        		return realMiddle;
+        	else if (nums[realMiddle] < target)
+        		l = middle + 1;
+        	else
+        		r = middle - 1;
+        }
+
         return -1;
     }
 };
