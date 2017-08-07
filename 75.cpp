7ee67e567a369a0ee9b158a336daf03f2b2f565b
Chiao Fu <joannelovecat@gmail.com>
2017-08-07 16:28:18 +0800

    one-pass

--- 75.cpp
+++ 75.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 = 0, count1 = 0, count2 = 0;
-        for (int i : nums) {
-            switch(i) {
-                case 0:
-                    count0 ++;
-                    break;
-                case 1:
-                    count1 ++;
-                    break;
-                case 2:
-                    count2 ++;
-                    break;
-            }
+        vector<int> flag(3, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            int val = nums[i];
+            nums.erase(nums.begin() + i);
+            nums.insert(nums.begin() + flag[val], val);
+            for (int j = val; j <= 2; j++)
+                flag[j] ++;
         }
-        for (int i = 0; i < count0; i++)
-            nums[i] = 0;
-        for (int i = count0; i < count0 + count1; i++)
-            nums[i] = 1;
-        for (int i = count0 + count1; i < count0 + count1 + count2; i++)
-            nums[i] = 2;
     }
 };
