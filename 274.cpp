26c843c3a5ef1e9dbf3978969194023729336ab3
Chiao Fu <joannelovecat@gmail.com>
2017-09-07 18:43:32 +0800

    O(logN) time

--- 274.cpp
+++ 274.cpp
@@ -5,10 +5,17 @@ public:
         if (citations.size() == 0)
         	return 0;
 
-        int n = citations.size(), result = 0;
+        // find the minimum index such that citations[index] >= citations.size() - index, result = citations.size() - index
         sort(citations.begin(), citations.end());
-        for (int i = 0; i < n; i++)
-        	result = max(result, min(citations[i], n - i));
-        return result;
+        int n = citations.size(), l = 0, r = n - 1;
+
+        while (l <= r) {
+        	int mid = (l + r) / 2;
+        	if (citations[mid] < n - mid)
+        		l = mid + 1;
+        	else
+        		r = mid - 1;
+        }
+        return (n - l);
     }
 };
--- 275.cpp
+++ 275.cpp
@@ -2,13 +2,16 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size(), result = 0;
-        for (int i = 0; i < n; i++) {
-        	if (min(citations[i], n - i) >= result)
-        		result = min(citations[i], n - i);
+    	// find the minimum index such that citations[index] >= citations.size() - index, result = citations.size() - index
+        int n = citations.size(), l = 0, r = n - 1;
+
+        while (l <= r) {
+        	int mid = (l + r) / 2;
+        	if (citations[mid] < n - mid)
+        		l = mid + 1;
         	else
-        		return result;
+        		r = mid - 1;
         }
-        return result;
+        return (n - l);
     }
 };
