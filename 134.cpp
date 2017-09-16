71033e6f50752919dcc382ac99829c4567ee2af7
Chiao Fu <joannelovecat@gmail.com>
2017-09-17 02:29:41 +0800

    O(n)

--- 134.cpp
+++ 134.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        vector<int> nums;
-        int n = gas.size(), minNum = 0, minFlag;
+        int n = gas.size(), l = 0, r = n - 1, sum = 0;
 
-        for (int i = 0; i < n; i++) {
-        	nums.push_back(gas[i] - cost[i]);
-        	if (nums.back() <= minNum) {
-        		minNum = nums.back();
-        		minFlag = i;
-        	}
+        while (l < r) {
+            if (sum + gas[l] - cost[l] >= 0) {
+                sum += (gas[l] - cost[l]);
+                l ++;
+            }
+            else {
+                sum += (gas[r] - cost[r]);
+                r --;
+            }
         }
-
-        for (int i = 0; i < n; i++) {
-        	bool complete = false;
-        	int temp = 0;
-        	for (int j = 0; j < n; j++) {
-        		temp += nums[(i + minFlag + 1 + j) % n];
-        		if (temp < 0)
-        			break;
-        		if (j == n - 1)
-        			return ((i + minFlag + 1) % n);
-        	}
-        }
-        return -1;
+        return (sum + gas[l] - cost[l] >= 0) ? (l + 1) % n : -1;
     }
 };
