7827f762c199603d44a967717d5ba6dfeed1ee08
Chiao Fu <joannelovecat@gmail.com>
2017-09-29 16:51:10 -0700

    no modifying board value

--- 419.cpp
+++ 419.cpp
@@ -6,19 +6,9 @@ public:
         int count = 0, n = board.size(), m = board[0].size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'X') {
+                // count the top-left point of boats
+                if (board[i][j] == 'X' && (i == 0 || board[i - 1][j] == '.') && (j == 0 || board[i][j - 1] == '.'))
                     count ++;
-                    int curR = i, curC = j;
-                    board[curR][curC] = 'x';
-                    while (curR + 1 < n && board[curR + 1][curC] == 'X') {
-                        board[curR + 1][curC] = 'x';
-                        curR ++;
-                    }
-                    while (curC + 1 < m && board[curR][curC + 1] == 'X') {
-                        board[curR][curC + 1] = 'x';
-                        curC ++;
-                    }
-                }
             }
         }
 
