63e60dbc75d86d4a7a203c0fdcaa09285ad3b25a
Chiao Fu <joannelovecat@gmail.com>
2017-10-03 11:25:45 -0700

    O(nlogn)

--- 300.cpp
+++ 300.cpp
@@ -4,15 +4,22 @@ public:
     	if (nums.size() == 0)
     		return 0;
     	
-    	int n = nums.size(), maxLength = 1;
-        vector<int> ISlength(n, 1);
-        for (int i = 1; i < n; i++) {
-        	for (int j = i - 1; j >= 0; j--) {
-        		if (nums[i] > nums[j])
-        			ISlength[i] = max(ISlength[i], ISlength[j] + 1);
-        	}
-        	maxLength = max(maxLength, ISlength[i]);
-        }
-        return maxLength;
+    	vector<int> IS;
+    	IS.push_back(nums[0]);
+    	for (int i = 1; i < nums.size(); i++) {
+    		int l = 0, r = IS.size() - 1;
+	        while (l <= r) {
+	            int mid = (l + r) / 2;
+	            if (IS[mid] < nums[i])
+	                l = mid + 1;
+	            else
+	                r = mid - 1;
+	        }
+	        if (l < IS.size())
+	        	IS[l] = nums[i];
+	        else
+	        	IS.push_back(nums[i]);
+    	}
+    	return IS.size();
     }
 };
