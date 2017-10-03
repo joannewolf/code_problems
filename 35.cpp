2fe550bef2b22d280da60555e0c8fdff510be4ce
Chiao Fu <joannelovecat@gmail.com>
2017-10-02 20:39:16 -0700

    O(log n)

--- 35.cpp
+++ 35.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if (nums.size() == 0 || target < nums[0])
-        	return 0;
-
-        for (int i = 0; i < nums.size() - 1; i++) {
-        	if (nums[i] == target)
-        		return i;
-        	if (nums[i] < target && nums[i + 1] > target)
-        		return (i + 1);
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
         }
-        return (nums.back() == target) ? nums.size() - 1 : nums.size();
+        return l;
     }
 };
