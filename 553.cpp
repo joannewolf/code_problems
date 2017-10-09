8226d4fd10446f3c68a6b20b023af52c05f17420
Chiao Fu <joannelovecat@gmail.com>
2017-10-09 00:10:12 -0700

    using math, O(n)

--- 553.cpp
+++ 553.cpp
@@ -1,37 +1,14 @@
-#include <limits>
 class Solution {
-private:
-	typedef struct {
-		float maxNum, minNum;
-		string maxStr, minStr;
-	} T;
-	T getOptimalDivision(vector<int> nums) {
-		T t;
-		if (nums.size() == 1) {
-			t.maxNum = nums[0];
-			t.minNum = nums[0];
-			t.maxStr = to_string(nums[0]);
-			t.minStr = to_string(nums[0]);
-			return t;
-		}
-		t.maxNum = numeric_limits<float>::min();
-		t.minNum = numeric_limits<float>::max();
-		for (int i = 1; i < nums.size(); i++) {
-			T left = getOptimalDivision(vector<int>(nums.begin(), nums.begin() + i));
-			T right = getOptimalDivision(vector<int>(nums.begin() + i, nums.end()));
-			if (t.minNum > left.minNum / right.maxNum) {
-				t.minNum = left.minNum / right.maxNum;
-				t.minStr = left.minStr + "/" + ((i != nums.size() - 1) ? "(" : "") + right.maxStr + ((i != nums.size() - 1) ? ")" : "");
-			}
-			if (t.maxNum < left.maxNum / right.minNum) {
-				t.maxNum = left.maxNum / right.minNum;
-				t.maxStr = left.maxStr + "/" + ((i != nums.size() - 1) ? "(" : "") + right.minStr + ((i != nums.size() - 1) ? ")" : "");
-			}
-		}
-		return t;
-	}
 public:
     string optimalDivision(vector<int>& nums) {
-        return getOptimalDivision(nums).maxStr;
+    	if (nums.size() == 1)
+    		return to_string(nums[0]);
+    	if (nums.size() == 2)
+    		return (to_string(nums[0]) + "/" + to_string(nums[1]));
+    	string result = to_string(nums[0]) + "/(";
+    	for (int i = 1; i < nums.size() - 1; i++)
+    		result += (to_string(nums[i]) + "/");
+    	result += (to_string(nums.back()) + ")");
+    	return result;
     }
 };
