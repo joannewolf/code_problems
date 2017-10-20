8512989792ebca98b752c493da938ff084bc98ba
Chiao Fu <joannelovecat@gmail.com>
2017-10-20 01:43:19 -0700

    by iteration

--- 144.cpp
+++ 144.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -12,10 +13,18 @@ public:
     vector<int> preorderTraversal(TreeNode* root) {
     	if (root == NULL)
     		return vector<int>();
-        vector<int> leftResult = preorderTraversal(root -> left);
-        vector<int> rightResult = preorderTraversal(root -> right);
-        leftResult.insert(leftResult.begin(), root -> val);
-        leftResult.insert(leftResult.end(), rightResult.begin(), rightResult.end());
-        return leftResult;
+    	vector<int> result;
+    	stack<TreeNode*> st; 
+    	st.push(root);
+    	while (!st.empty()) {
+    		TreeNode *temp = st.top();
+    		st.pop();
+    		result.push_back(temp -> val);
+    		if (temp -> right != NULL)
+    			st.push(temp -> right);
+    		if (temp -> left != NULL)
+    			st.push(temp -> left);
+    	}
+        return result;
     }
 };
