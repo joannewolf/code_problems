60bfd5846a334bf8be602d0ec347e9b59222ed96
Chiao Fu <joannelovecat@gmail.com>
2017-10-24 01:40:40 -0700

    O(Nlogk)

--- 23.cpp
+++ 23.cpp
@@ -8,30 +8,36 @@
  * };
  */
 class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
+private:
+    ListNode* merge2Lists(ListNode* &list1, ListNode* &list2) {
         ListNode* newHead = new ListNode(0), *current = newHead;
-        for (int i = 0; i < lists.size(); i++) {
-            if (lists[i] == NULL) {
-                lists.erase(lists.begin() + i);
-                i--;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1 -> val < list2 -> val) {
+                current -> next = list1;
+                list1 = list1 -> next;
             }
-        }
-        while (!lists.empty()) {
-            int minMalue = INT_MAX, flag = -1;
-            for (int i = 0; i < lists.size(); i++) {
-                if (lists[i] -> val < minMalue) {
-                    minMalue = lists[i] -> val;
-                    flag = i;
-                }
+            else {
+                current -> next = list2;
+                list2 = list2 -> next;
             }
-            current -> next = lists[flag];
-            if (lists[flag] -> next == NULL)
-                lists.erase(lists.begin() + flag);
-            else
-                lists[flag] = lists[flag] -> next;
             current = current -> next;
         }
+        if (list1 != NULL)
+            current -> next = list1;
+        else if (list2 != NULL)
+            current -> next = list2;
         return (newHead -> next);
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0)
+            return NULL;
+        while (lists.size() > 1){
+            for (int i = 1; i < lists.size(); i ++) {
+                lists[i - 1] = merge2Lists(lists[i], lists[i - 1]);
+                lists.erase(lists.begin() + i);
+            }
+        }
+        return lists[0];
     }
 };
