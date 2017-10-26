3fefad3b78b422e6eac744b98977350f6694c41f
Chiao Fu <joannelovecat@gmail.com>
2017-10-26 16:25:24 -0700

    O(n) greedy

--- 376.cpp
+++ 376.cpp
@@ -2,26 +2,24 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-    	if (nums.empty())
-    		return 0;
-    	int n = nums.size();
-        vector<int> dpUp(n, 1), dpDown(n, 1);
-        // dpUp[i]: the maximum wiggle length from nums[0] to nums[i], which the last step is wiggle up to nums[i]
-        for (int i = 1; i < n; i++) {
+        if (nums.empty())
+            return 0;
+    	int result = 1, direction = 0;
+        // direction, 1: increasing, -1: decreasing
+        for (int i = 1; i < nums.size(); i++) {
             if (nums[i] > nums[i - 1]) {
-                dpUp[i] = dpDown[i - 1] + 1;
-                dpDown[i] = dpDown[i - 1];
+                if (direction != 1) {
+                    result ++;
+                }
+                direction = 1;
             }
             else if (nums[i] < nums[i - 1]) {
-                dpDown[i] = dpUp[i - 1] + 1;
-                dpUp[i] = dpUp[i - 1];
-            }
-            else {
-                dpUp[i] = dpUp[i - 1];
-                dpDown[i] = dpDown[i - 1];
+                if (direction != -1) {
+                    result ++;
+                }
+                direction = -1;
             }
         }
-        
-        return max(dpUp[n - 1], dpDown[n - 1]);
+        return result;
     }
 };
