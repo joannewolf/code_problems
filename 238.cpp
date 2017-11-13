c9eb78d866268e16d0a098f52c9f9122a31bc190
Chiao Fu <joannelovecat@gmail.com>
2017-11-12 19:38:21 -0800

    O(n) without division

--- 238.cpp
+++ 238.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        long long product = 1;
-        vector<int> result(nums.size(), 0);
-        vector<int> zeroIndex;
+        vector<int> result(nums.size());
 
-        for (int i = 0; i < nums.size(); i++) {
-        	if (nums[i] == 0)
-        		zeroIndex.emplace_back(i);
-        	else
-        		product *= nums[i];
+        for (int i = 0, temp = 1; i < nums.size(); i++) {
+        	result[i] = temp;
+        	temp *= nums[i];
         }
 
-        if (zeroIndex.size() == 1){
-        	result[zeroIndex[0]] = product;
+        for (int i = nums.size() - 1, temp = 1; i >= 0; i--) {
+        	result[i] *= temp;
+        	temp *= nums[i];
         }
-        else if (zeroIndex.size() == 0){
-			for (int i = 0; i < nums.size(); i++)
-				result[i] = product / nums[i];
-        }
+
         return result;
     }
 };
