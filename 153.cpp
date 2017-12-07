f9585eb17e53f6c426d8d6e189a5e7f48bc742fe
Chiao Fu <joannelovecat@gmail.com>
2017-12-07 02:09:21 -0800

    O(logn)

--- 153.cpp
+++ 153.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        for (int i = 0; i < nums.size() - 1; i++) {
-        	if (nums[i] > nums[i + 1])
-        		return nums[i + 1];
+        int l = 0, r = nums.size() - 1;
+
+        while (l < r) {
+        	if (nums[l] < nums[r])
+        		return nums[l];
+
+        	int middle = (l + r) / 2;
+        	if (nums[l] <= nums[middle])
+        		l = middle + 1;
+        	else
+        		r = middle;
         }
-    	return nums[0];
+        return nums[l];
     }
 };
