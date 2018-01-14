6df1b5fb04b9730f1206d3c9b76d5be9209b1c5c
Chiao Fu <joannelovecat@gmail.com>
2018-01-13 21:07:14 -0800

    all solutions

--- 219.cpp
+++ 219.cpp
@@ -1,19 +1,36 @@
-#include <map>
+// O(N), use map to record index
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        if (k <= 0)
-        	return false;
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+		if (k <= 0)
+			return false;
 
-        map<int, int> pos;
-        for(int i = 0; i < nums.size(); i++) {
-        	if (pos.find(nums[i]) != pos.end() && i - pos[nums[i]] <= k)
-       			// appeared, compare the distance
-       			return true;
-        	else
-    			// add new entry in map, or update the index
-    			pos[nums[i]] = i;
-        }
-        return false;
-    }
+		map<int, int> pos;
+		for(int i = 0; i < nums.size(); i++) {
+			if (pos.find(nums[i]) != pos.end() && i - pos[nums[i]] <= k)
+				// appeared, compare the distance
+				return true;
+			else
+				// add new entry in map, or update the index
+				pos[nums[i]] = i;
+		}
+		return false;
+	}
+};
+
+// O(N), use set to record window with size k
+class Solution {
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+		unordered_set<int> set;
+		for (int i = 0; i < nums.size(); i++) {
+			if (set.find(nums[i]) != set.end())
+				return true;
+
+			set.insert(nums[i]);
+			if (i >= k)
+				set.erase(nums[i - k]);
+		}
+		return false;
+	}
 };
--- 3.cpp
+++ 3.cpp
@@ -1,19 +1,63 @@
+// O(N), use map
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        string tempStr = "";
-        int maxLength = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int temp = tempStr.find(s[i]);
-            if (temp == -1)
-                tempStr.push_back(s[i]);
-            else {
-                maxLength = max(maxLength, (int)(tempStr.length()));
-                tempStr = tempStr.substr(temp + 1);
-                tempStr.push_back(s[i]);
-            }
-        }
+	int lengthOfLongestSubstring(string s) {
+		int startIndex = 0, maxLength = 0;
+		map<char, int> index;
 
-        return max(maxLength, (int)(tempStr.length()));
-    }
+		for (int i = 0; i < s.length(); i++) {
+			if (index.find(s[i]) != index.end())
+				startIndex = max(startIndex, index[s[i]] + 1);
+			index[s[i]] = i;
+			maxLength = max(maxLength, i - startIndex + 1);
+		}
+
+		return maxLength;
+	}
+};
+
+// O(N), use set and two pointers
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int startIndex = 0, currentIndex = 0;
+		int maxLength = 0;
+		set<char> chars;
+
+		while (currentIndex < s.length()) {
+			if (chars.find(s[currentIndex]) == chars.end()) {
+				chars.emplace(s[currentIndex]);
+				currentIndex ++;
+				maxLength = max(maxLength, (int)chars.size());
+			}
+			else {
+				chars.erase(s[startIndex]);
+				startIndex ++;
+			}
+		}
+		return maxLength;
+	}
+};
+
+// O(N^2), use string.find()
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		string currentSubstr = "";
+		int maxLength = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			int index = currentSubstr.find(s[i]);
+			if (index == -1) {
+				currentSubstr.push_back(s[i]);
+				maxLength = max(maxLength, (int)currentSubstr.length());
+			}
+			else {
+				currentSubstr = currentSubstr.substr(index + 1);
+				currentSubstr.push_back(s[i]);
+			}
+		}
+
+		return maxLength;
+	}
 };
