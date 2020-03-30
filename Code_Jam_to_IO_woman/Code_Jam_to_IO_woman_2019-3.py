4b5888f39ecd211caf155cdfa89cfc1a143ff41f
joannewolf <joannelovecat@gmail.com>
2020-03-29 21:29:16 -0700

    .

---
+++ Code_Jam_to_IO_woman/Code_Jam_to_IO_woman_2019-3.py
@@ -0,0 +1,30 @@
+# https://codingcompetitions.withgoogle.com/codejamio/round/0000000000050fc5/0000000000054edd
+
+T = raw_input()
+for t in xrange(int(T)):
+    [x, y] = [int(a) for a in raw_input().split(' ')]
+    [x, y] = sorted([abs(x), abs(y)]) # x >= 0, y >= 1, x <= y
+
+    # row 0...i...x, col 0...j...y; only i <= j has value (expected steps)
+    ans = [[float(0)] * (y + 1) for _ in xrange(x + 2)]
+    # ans[0][1] = 0.5 * 1 + 0.5 * (1 + ans[1][1])
+    # ans[1][1] = 0.5 * (1 + ans[0][1]) + 0.5 * (1 + ans[0][1])
+    ans[0][1] = 3.0
+    ans[1][1] = 4.0
+
+    # if i == 0, ans[0][j] = 0.5 * (1 + ans[0][j - 1]) + 0.5 * (1 + ans[1][j])
+    #                      = 0.5 * (1 + ans[0][j - 1]) + 0.5 * (1 + (0.5 * (1 + ans[0][j]) + 0.5 * (1 + ans[1][j - 1])))
+    #                      = 3/2 + 1/2 * ans[0][j - 1] + 1/4 * ans[0][j] + 1/4 * ans[1][j - 1]
+    #            ans[0][j] = 4/3 * (3/2 + 1/2 * ans[0][j - 1] + 1/4 * ans[1][j - 1])
+    #                      = 2 + 2/3 * ans[0][j - 1] + 1/3 * ans[1][j - 1]
+    # if i != 0, ans[i][j] = 0.5 * (1 + ans[i - 1][j]) + 0.5 * (1 + ans[i][j - 1])
+    for j in xrange(2, y + 1):
+        ans[0][j] = 2.0 + 2.0/3.0 * ans[0][j - 1] + 1.0/3.0 * ans[1][j - 1]
+        ans[1][j] = 1.0 + 0.5 * ans[0][j] + 0.5 * ans[1][j - 1]
+        for i in xrange(2, min(x + 1, j + 1)):
+            if i == j:
+                ans[i][j] = ans[i - 1][j] + 1.0
+            else:
+                ans[i][j] = 1.0 + 0.5 * ans[i - 1][j] + 0.5 * ans[i][j - 1]
+
+    print "Case #{}: {}".format(t + 1, ans[x][y])
---
+++ Code_Jam_to_IO_woman/Code_Jam_to_IO_woman_2020-2.py
@@ -0,0 +1,43 @@
+# https://codingcompetitions.withgoogle.com/codejamio/round/000000000019ff03/00000000001b5cd7
+
+T = raw_input()
+for t in xrange(int(T)):
+    N = int(raw_input())
+    A = [int(a) for a in raw_input().split(' ')]
+    ans = []
+    pair = {}
+
+    if N % 2 == 1:
+        A.insert(0, N + 1)
+        ans = ['X'] * (N + 2)
+    else:
+        ans = ['X'] * (N + 1)
+
+    # pair (A1, A2), (A3, A4), ..., and each pair has to be one L and one R
+    # also pair (1, 2), (3, 4), ..., and each pair has to be one L and one R
+    for i in range(1, len(ans), 2):
+        pair[i] = {i + 1}
+        pair[i + 1] = {i}
+    
+    for i in range(0, len(A), 2):
+        pair[A[i]].add(A[i + 1])
+        pair[A[i + 1]].add(A[i])
+
+    toBeChecked = []
+    while pair:
+        if not toBeChecked:
+            currentKey = pair.keys()[0]
+            ans[currentKey] = 'L'
+            for value in pair[currentKey]:
+                ans[value] = 'R'
+            toBeChecked.extend(pair[currentKey])
+            pair.pop(currentKey)
+        else:
+            currentKey = toBeChecked.pop(0)
+            for value in pair[currentKey]:
+                if ans[value] == 'X':
+                    ans[value] = 'L' if ans[currentKey] == 'R' else 'R'
+                    toBeChecked.append(value)
+            pair.pop(currentKey)
+
+    print "Case #{}: {}".format(t + 1, ''.join(ans[1 : N + 1]))
